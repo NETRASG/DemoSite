fceacbbb5e3efa867797a49b09148d06708ee426
NETRASG <netra4al@gmail.com>
2023-05-08 15:05:58 +0530

    header implementation

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,38 +1,63 @@
 import React from "react";
 import "./Header.scss";
+import down_arrow_head from "../../utils/svg_files/down_arrow_head.svg";
+import greatlearning_brand from "../../utils/svg_files/greatlearning_brand.svg";
+
 import { Link } from "react-router-dom";
 
-
-const Header = (props:any) => {
+const Header = (props: any) => {
   return (
-    <nav className="navbar navbar-expand-md navbar-light bg-light">
-  <div className=" container-fluid">
-    <a className="navbar-brand" href="#">Logo</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <Link to='/' className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </Link>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-      </ul>
-      <ul className="navbar-nav ml-auto mb-2 mb-lg-0 ">
-        {/* <Link to='/' className="nav-item"> */}
-          <a className="nav-link active" onClick={props.handleOnClick} aria-current="page" href="#">Sign Up</a>
-        {/* </Link> */}
-        <Link to='/login' className="nav-item">
-          <a className="nav-link" href="#">Login</a>
-        </Link>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-
+    <nav className="navbar navbar-expand-md main-header">
+      <div className=" container-fluid">
+        <a className="navbar-brand" href="#">
+          <img src={greatlearning_brand} height="35px" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 left-header">
+            <Link to="/" className="nav-item">
+              <div className="explore-button">
+                <span> Explore Programs </span>
+                <img src={down_arrow_head} alt="" />
+              </div>
+            </Link>
+            <Link to="/" className="nav-item">
+              <div>Career Support</div>
+            </Link>
+            <Link className="nav-item" to="/">
+            <div>Success Stories</div>
+            </Link>
+            <Link className="nav-item" to="/">
+            <div>Study Abroad</div>
+            </Link>
+            <Link className="nav-item" to="/">
+            <div>Enterprise Solutions</div>
+            </Link>
+            <Link className="nav-item" to="/">
+            <div>All Programs</div>
+            </Link>
+            <Link className="nav-item" to="/">
+            <div>More</div> 
+            </Link>
+          </ul>
+          <ul className="navbar-nav ml-auto mb-2 mb-lg-0 ">
+            <Link to="" className="" onClick={props.handleOnClick}>
+            <div>Sign Up</div>
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
